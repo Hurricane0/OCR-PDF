b4e6276945b6b66f256d94195895b98573567066
Hurricane0 <nikitadv777@gmail.com>
2020-03-08 18:34:29 +0200

    Russina language support + Styling

--- app.js
+++ app.js
@@ -19,31 +19,63 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage }).single("avatar");
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 //Routes
 app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.post("/lang", (req, res) => {
+  console.log(req.body.language);
+});
+
 app.post("/upload", (req, res) => {
+  upload(req, res, err => {
+    try {
+      fs.readFile(`./uploads/${req.file.originalname}`, (err, data) => {
+        if (err) return console.log("Something went wrong: ", err);
+        //worker recognizing
+        worker
+          .recognize(data, "eng", { tessjs_create_pdf: "1" })
+          .progress(progress => {
+            // console.log(progress);
+          })
+          .then(result => {
+            res.redirect("/download");
+          })
+          .finally(() => worker.terminate());
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  });
+});
+app.post("/russian_upload", (req, res) => {
   upload(req, res, err => {
     fs.readFile(`./uploads/${req.file.originalname}`, (err, data) => {
       if (err) return console.log("Something went wrong: ", err);
-
       //worker recognizing
       worker
         .recognize(data, "rus", { tessjs_create_pdf: "1" })
         .progress(progress => {
-          console.log(progress);
+          // console.log(progress);
         })
         .then(result => {
-          res.send(result.text);
+          res.redirect("/download");
         })
         .finally(() => worker.terminate());
     });
   });
 });
 
+app.get("/download", (req, res) => {
+  const file = `${__dirname}/tesseract.js-ocr-result.pdf`;
+  res.sendFile(file);
+});
+
 //Start up the server
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
