5a245d3d98601935d83a906421bfdd448acf43d4
Hurricane0 <nikitadv777@gmail.com>
2020-03-08 15:00:26 +0200

    Initial

---
+++ app.js
@@ -0,0 +1,49 @@
+//Imports declaring
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const multer = require("multer");
+
+const { TesseractWorker } = require("tesseract.js");
+const worker = new TesseractWorker();
+
+//Storage declaring
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({ storage: storage }).single("avatar");
+
+app.set("view engine", "ejs");
+
+//Routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/upload", (req, res) => {
+  upload(req, res, err => {
+    fs.readFile(`./uploads/${req.file.originalname}`, (err, data) => {
+      if (err) return console.log("Something went wrong: ", err);
+
+      //worker recognizing
+      worker
+        .recognize(data, "rus", { tessjs_create_pdf: "1" })
+        .progress(progress => {
+          console.log(progress);
+        })
+        .then(result => {
+          res.send(result.text);
+        })
+        .finally(() => worker.terminate());
+    });
+  });
+});
+
+//Start up the server
+const PORT = 5000 || process.env.PORT;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
